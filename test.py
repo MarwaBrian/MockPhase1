7dbb21b6dea54c64b027a44be1c4853499d51b9a
MarwaBrian <marwabrian7@gmail.com>
2025-06-18 07:58:49 +0300

    pandas functions

---
+++ test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np 
+
+
+...
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+""""""
+
+
+def capitalize(name):
+    ...
+
+def punctuate(name):
+    ...
